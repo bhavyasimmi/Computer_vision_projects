98a12fbd231372098ab7fe1c2c3f0e5f5386d200
bhavyasimmi <bhavya19agrawal@gmail.com>
2018-06-02 16:09:26 +0530

    making own image taking portions from user_input

---
+++ user_image.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import cv2
+import time
+
+image = input("which image do you want to open\n")
+
+#read the user image by default in colorful mode
+img = cv2.imread(image)
+
+#loading the actual image
+print("actual image is\n")
+cv2.imshow("actual_image",img)
+cv2.waitKey(4)
+cv2.destroyAllWindows()
+
+time.sleep(1)
+shape = img.shape
+print("actual size of image is :",shape)
+
+i = input("how much part of the image do you want to see\n")
+
+if i == "1/2":
+	part = 2
+elif i == "1/3":
+	part = 3
+elif i == "1/4":
+	part = 4
+elif i == "1/8":
+	part = 8
+
+
+# partition of image as per user partitions
+user_image = img[0:int(shape[0]/part),0:int(shape[1]/part)]
+
+print("the image after partition is\n")
+cv2.imshow("user_image",user_image)
+
+#time.sleep(4)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
